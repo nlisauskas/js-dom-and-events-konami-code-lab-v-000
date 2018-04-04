301207148bfc5280b67da9f9078445534ebd8036
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-04 02:37:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
 
-  function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail);
 
   if (key === code[index]) {
@@ -17,6 +17,6 @@ function init() {
   } else {
     index = 0;
   }
-}
+})
 
 }
